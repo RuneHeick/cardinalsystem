f757d989914023233bb033f425d03bf876090134
ArbjergHeick@gmail.com <ArbjergHeick@gmail.com@b42d6c53-c84a-9462-49f6-eda5ba912c26>
2014-12-22 10:06:36 +0000

    compiler test added

---
+++ trunk/CompilerTest/CompilerTest/TestCompile.cs
@@ -0,0 +1,30 @@
+﻿using Schare;
+
+namespace CompilerTest
+{
+    public class B: Ishare
+    {
+        
+        
+        public static double k = 7;
+
+        static B()
+        {
+            k = 800;
+        }
+
+
+
+        public string Test
+        {
+            get
+            {
+                return "Test2"; 
+            }
+            set
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
---
+++ trunk/CompilerTest/Schare/Ishare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schare
+{
+    public interface Ishare
+    {
+        string Test { get; set; } 
+
+    }
+}
