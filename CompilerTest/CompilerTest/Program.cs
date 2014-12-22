f83ef1e128ce2b2fb421aa7da5635a71c804ef30
ArbjergHeick@gmail.com <ArbjergHeick@gmail.com@b42d6c53-c84a-9462-49f6-eda5ba912c26>
2014-12-22 10:06:36 +0000

    compiler test added

---
+++ CompilerTest/CompilerTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection; 
+
+namespace CompilerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateExecutable = false;
+            parameters.GenerateInMemory = false;
+            parameters.ReferencedAssemblies.Add("Schare.dll");
+            parameters.OutputAssembly = "AutoGen.dll";
+            parameters.IncludeDebugInformation = true;
+
+
+            var pro = CodeDomProvider.CreateProvider("CSharp");
+            CompilerResults r = pro.CompileAssemblyFromFile(parameters, "TestCompile.cs");
+            
+
+
+            //verify generation
+            Schare.Ishare item = Activator.CreateInstance(Assembly.LoadFrom("AutoGen.dll").GetType("CompilerTest.B")) as Schare.Ishare;
+
+
+            Console.WriteLine(item.Test); 
+
+
+        }
+    }
+}
